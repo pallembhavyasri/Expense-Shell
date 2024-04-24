db44c01c13a91ab71b705935e473aa64bed24baf
Bhavya Sri P <pallembhavya1@gmail.com>
2024-04-24 15:10:29 +0530

    backend shell script

--- Backend.sh
+++ Backend.sh
@@ -30,13 +30,13 @@ else
 fi
 
 
-dnf module disable nodejs -y &>>logfile
+dnf module disable nodejs -y &>>$logfile
 validate $? "Disabling nodejs"
 
-dnf module enable nodejs:20 -y &>>logfile
+dnf module enable nodejs:20 -y &>>$logfile
 validate $? "Enabling nodejs:20"
 
-dnf install nodejs -y &>>logfile
+dnf install nodejs -y &>>$logfile
 validate $? "Installing nodejs"
 
 # useradd expense &>>logfile
@@ -60,7 +60,7 @@ fi
 # The above will fail after first run because it is idempotent to overcome we just need to keep -p before mkdir so after success 
 # if we run again to will skip and doen't throw error 
 
-mkdir -p /app &>>logfile
+mkdir -p /app &>>$logfile
 validate $? "Craeting the folder"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>logfile
@@ -69,32 +69,32 @@ validate $? "Downloading code from backend"
 
 cd /app 
 rm -rf /app/* 
-unzip /tmp/backend.zip &>>logfile
+unzip /tmp/backend.zip &>>$logfile
 validate $? "Unziping the backend code"
 
-npm install &>>logfile
+npm install &>>$logfile
 validate $? "Installing the dependencies"
 
 cp /home/ec2-user/Expense-Shell/Backend.sh /etc/systemd/system/backend.service &>>logfile
 validate $? "Copied backend.service"
 
-systemctl daemon-reload &>>logfile
+systemctl daemon-reload &>>$logfile
 validate $? "Reloading the backend"
 
-systemctl start backend &>>logfile
+systemctl start backend &>>$logfile
 validate $? "Starting backend"
 
-systemctl enable backend &>>logfile
+systemctl enable backend &>>$logfile
 validate $? "Enabling the backend"
 
 
-dnf install mysql -y &>>logfile
+dnf install mysql -y &>>$logfile
 validate $? "Installing mysql"
 
 mysql -h db.bhavya.store -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>logfile
 validate $? "Schema loading"
 
-systemctl restart backend &>>logfile
+systemctl restart backend &>>$logfile
 validate $? "Restarting backend"
 
 
