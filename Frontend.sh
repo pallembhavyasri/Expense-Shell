010f9f25a0d1740cf51dab46387dd0b9dfe2c0ab
Bhavya Sri P <pallembhavya1@gmail.com>
2024-04-25 11:03:13 +0530

    backend shell script

--- Frontend.sh
+++ Frontend.sh
@@ -1,30 +1,8 @@
 #!/bin/bash
 
-user=$(id -u)
-timestamp=$(date +%F-%H-%M-%S)
-scriptname=$( echo $0 | cut -d "." -f1)
-logfile=/tmp/$scriptname-$timestamp.log
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+sourse ./Common.sh
 
-validate(){
-    if [ $1 -eq 0 ]
-    then 
-        echo -e "$2...$G Success $N"
-    else
-        echo -e "$2...$R Failure $N"
-    fi 
-}
-
-if [ $user -eq 0 ]
-then    
-    echo -e "user is having $G root access $N" 
-else
-    echo -e " $R Run with root access $N"
-    exit 1
-fi
+Check_root()
 
 dnf install nginx -y &>>$logfile
 validate $? "Installing Nginx"
